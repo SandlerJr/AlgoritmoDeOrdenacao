0aa796c47aa53dff3e83ccbf4f7437721b0250db
SandlerJr <sandlerjunin@gmail.com>
2024-03-04 19:03:02 -0300

    refatorando array de livros para deixar nossa lista mais completa

--- problema1/index.js
+++ problema1/index.js
@@ -1,12 +1,12 @@
-const precoLivros = [25, 15, 45, 50, 30, 20];
+const livros = require('./listalivros'); 
 
 let atual = 0;
 let maisBarato = 0;
 
-for ( let atual = 0; atual < precoLivros.length; atual++){
-    if (precoLivros[atual] < precoLivros[maisBarato]){
+for ( let atual = 0; atual < livros.length; atual++){
+    if (livros[atual].preço < livros[maisBarato].preço){
         maisBarato = atual;
     }
 }
 
-console.log(`O livro mais barato está na pocição ${maisBarato} e custa R$${precoLivros[maisBarato]},00`);
+console.log(`O livro mais barato é o ${livros[maisBarato].titulo} que está na pocição ${maisBarato} da nossa lista e custa R$${livros[maisBarato].preço},00`);
---
+++ problema1/listalivros.js
@@ -0,0 +1,28 @@
+const precoLivros = [
+    {
+        titulo: 'JavaScript', 
+        preço:25
+    }, 
+    {
+        titulo: 'PHP',
+        preço : 15
+    }, 
+    {
+        titulo: 'java', 
+        preço: 45
+    },
+    {
+        titulo: 'Elixir', 
+        preço: 50
+    },
+    {
+        titulo: 'Go',
+        preço: 30
+    },
+    {
+        titulo: 'Phython',
+        preço: 20
+    },
+];
+
+module.exports = precoLivros;
